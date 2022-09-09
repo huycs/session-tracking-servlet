cf4940d418dcd494a472cf78b451f622f587ef66
huycs <voquanghuy.bk@gmail.com>
2022-09-09 16:08:42 +0700

    first commit

---
+++ src/main/java/demo/Session.java
@@ -0,0 +1,53 @@
+package demo;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+/**
+ * Servlet implementation class Session
+ */
+public class Session extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Session() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		
+		Cart cart = (Cart)session.getAttribute("cart");
+		
+		if(cart == null) {
+			cart = new Cart();
+		}		
+		
+		cart.setTotalItems(7);
+		
+		session.setAttribute("cart", cart);
+		
+		getServletContext().getRequestDispatcher("/showcart.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
